7e7b58482503b86bac84fc02b2efb3b797dcb9fe
José G. R. Launes <josegrlopes@gmail.com>
2024-07-05 10:51:22 +0200

    added JS file with first DOM Manipulation

---
+++ script.js
@@ -0,0 +1,7 @@
+const pageBody = document.querySelector(".page-body");
+
+const divDescription = document.createElement('div');
+
+divDescription.textContent = 'Data Science & Web Development';
+
+pageBody.appendChild(divDescription);
